57a5990f753881511cc759d4a0db3b13abce3bc9
1211656 <105354083+1211656@users.noreply.github.com>
2025-03-03 18:13:05 +0000

    update on image background and other stuff

--- src/image_rendering.js
+++ src/image_rendering.js
@@ -1,14 +1,14 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     // Seleciona o elemento pelo ID ou classe
-//     const emailBox = document.querySelector('.logo');
+document.addEventListener("DOMContentLoaded", () => {
+    // Seleciona o elemento pelo ID ou classe
+    const emailBox = document.querySelector('.logo-container');
 
-//     // Cria um novo elemento <img>
-//     const img = document.createElement('img');
-//     img.src = './1.png'; // Define o caminho da imagem
-//     img.alt = 'Description of the image'; // Define o texto alternativo da imagem
-//     img.style.width = '10vw'; // Define a largura da imagem
-//     img.style.height = '10vh'; // Define a altura da imagem
-
-//     // Adiciona a imagem ao elemento selecionado
-//     emailBox.appendChild(img);
-// });
+    // Cria um novo elemento <img>
+    const img = document.createElement('img');
+    img.src = './8.png'; // Define o caminho da imagem
+    img.alt = 'Description of the image'; // Define o texto alternativo da imagem
+    img.style.width = '270px'; // Define a largura da imagem
+    img.style.height = '270px'; // Define a altura da imagem
+    img.classList.add('zoomable'); // Adiciona a classe 'zoomable' à imagem
+    // Adiciona a imagem ao elemento selecionado
+    emailBox.appendChild(img);
+});
